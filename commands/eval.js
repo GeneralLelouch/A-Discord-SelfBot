3c849f8b9533019024723ffeae3f9f4298c55678
TherealPOKEGAMER <Aniketh470@gmail.com>
2017-09-17 15:31:20 +0530

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -1,16 +1,42 @@
 const Discord = require("discord.js");
 
-exports.run = async (client, msg, args) => {
-  const code = args.join(" ");
-  try {
-      const evaled = client.clean(await eval(code));
-      msg.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``
-      );
+exports.run = (client, message, args) => {
+  const clean = text => {
+    if (typeof(text) === 'string') {
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    } else {
+      return text;
+    }
   }
-  catch(err) {
-      msg.channel.send(`\`ERROR\` \`\`\`xl\n${client.clean(err)}\n\`\`\``);
+  try {
+    const args = message.content.split(' ').slice(1);
+    const code = args.join(' ');
+    let evaled = eval(code);
+    if (typeof evaled !== 'string')
+    evaled = require('util').inspect(evaled);
+    message.delete()
+    const embed = new Discord.RichEmbed()
+    .setTitle("**EVAL**")
+    .setAuthor(`${message.author.username}`, `${message.author.avatarURl}`)
+    .setDescription(`**EVAL INPUT:**\n\`\`\`${code}\`\`\`\n\n**EVAL OUTPUT:**\n\`\`\`${clean(evaled)}\`\`\``)
+    .setColor(0x00AE86)
+    .setFooter("A Discord.js Selfbot", "https://goo.gl/PheEXw")
+    .setTimestamp()
+
+    message.channel.send({embed});
+  } catch (err) {
+    const embed = new Discord.RichEmbed()
+    .setTitle("EVAL ERROR")
+    .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
+    .setDescription(`**ERROR**\`\`\`xl\n${clean(err)}\n\`\`\``)
+    .setColor(0x00AE86)
+    .setFooter("A Discord.js Selfbot", "https://goo.gl/PheEXw")
+    .setTimestamp()
+
+    message.channel.send({embed});
   }
 };
+
 
 exports.conf = {
   enabled: true,
