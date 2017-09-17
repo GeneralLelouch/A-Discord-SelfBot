93f03a71fb6adf5efd6b72fa604960eeef55eeb6
TherealPOKEGAMER <Aniketh470@gmail.com>
2017-09-17 15:13:17 +0530

    Add files via upload

---
+++ commands/eval.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, msg, args) => {
+  const code = args.join(" ");
+  try {
+      const evaled = client.clean(await eval(code));
+      msg.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``
+      );
+  }
+  catch(err) {
+      msg.channel.send(`\`ERROR\` \`\`\`xl\n${client.clean(err)}\n\`\`\``);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'eval',
+  description: 'Evaluates basic strings of JS',
+  usage: 'eval'
+};
